b6acd856bea62be428e74217e81b7e9246ef6977
nikhilmsharma <nikmsharma@gmail.com>
2024-09-13 09:32:28 +0530

    Added Files

--- src/main/java/org/cactus/ExampleResource.java
+++ src/main/java/org/cactus/ExampleResource.java
@@ -1,4 +1,4 @@
-package org.acme.micrometer;
+package org.cactus;
 
 import java.util.LinkedList;
 import java.util.NoSuchElementException;
@@ -16,23 +16,66 @@ import io.micrometer.core.instrument.Timer;
 @Produces("text/plain")
 public class ExampleResource {
 
+    private final MeterRegistry registry;
+
+    private final LinkedList<Long> list = new LinkedList<>();
+
+    ExampleResource(MeterRegistry registry) {
+        this.registry = registry;
+        registry.gaugeCollectionSize("example.list.size", Tags.empty(), list);
+    }
+
+    @GET
+    @Path("gauge/{number}")
+    public Long checkListSize(@PathParam("number") long number) {
+        if (number == 2 || number % 2 == 0) {
+            // add even numbers to the list
+            list.add(number);
+        } else {
+            // remove items from the list for odd numbers
+            try {
+                number = list.removeFirst();
+            } catch (NoSuchElementException nse) {
+                number = 0;
+            }
+        }
+        return number;
+    }
+
     @GET
     @Path("prime/{number}")
     public String checkIfPrime(@PathParam("number") long number) {
         if (number < 1) {
+            registry.counter("example.prime.number", "type", "not-natural")
+                    .increment();
             return "Only natural numbers can be prime numbers.";
         }
         if (number == 1) {
+            registry.counter("example.prime.number", "type", "one")
+                    .increment();
             return number + " is not prime.";
         }
         if (number == 2 || number % 2 == 0) {
+            registry.counter("example.prime.number", "type", "even")
+                    .increment();
             return number + " is not prime.";
         }
-        if (testPrimeNumber(number)) {
+        if (timedTestPrimeNumber(number)) {
+            registry.counter("example.prime.number", "type", "prime")
+                    .increment();
             return number + " is prime.";
         } else {
+            registry.counter("example.prime.number", "type", "not-prime")
+                    .increment();
             return number + " is not prime.";
         }
+    }
+
+    protected boolean timedTestPrimeNumber(long number) {
+        Timer.Sample sample = Timer.start(registry);
+        boolean result = testPrimeNumber(number);
+        sample.stop(registry.timer("example.prime.number.test", "prime", result + ""));
+        return result;
     }
 
     protected boolean testPrimeNumber(long number) {
